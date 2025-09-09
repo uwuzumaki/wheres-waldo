6ec9393d48bc1ff3a927429593acc12c5f87b281
Kris Wong <krischwong@gmail.com>
2025-09-08 23:23:30 -0400

    font

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import "./index.css";
 import routes from "./routes.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
