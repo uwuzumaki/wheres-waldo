8d8200611e6023d26349861eee1782cfad65c13a
Kris Wong <krischwong@gmail.com>
2025-09-15 23:12:15 -0400

    Added theme colours

---
+++ src/theme/index.jsx
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: { main: "#F9D142" },
+    secondary: { main: "#292826" },
+  },
+});
+
+export default theme;
