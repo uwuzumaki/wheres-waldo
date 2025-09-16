1b1dd5b58bd02d098522a60a184100a75cede848
Kris Wong <krischwong@gmail.com>
2025-09-15 23:11:32 -0400

    Added basic homepage

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,40 @@
-import { styled } from "@mui/material";
+import { styled, Button } from "@mui/material";
 
-const Base = styled("div")({
+const Base = styled("div")(({ theme }) => ({
   flex: 1,
+  backgroundColor: theme.palette.secondary.main,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+  color: "white",
+}));
+
+const Section = styled("div")({
+  width: "100%",
+  display: "flex",
+  justifyContent: "center",
+  marginLeft: "auto",
+  marginRight: "auto",
+});
+
+const CustomButton = styled(Button)({
+  marginLeft: "5%",
+  marginRight: "5%",
 });
 
 const Home = () => {
   return (
     <Base>
-      <div>456</div>
+      <Section>Username</Section>
+      <Section>
+        <CustomButton variant="contained">Picture A</CustomButton>
+        <CustomButton variant="contained">Picture B</CustomButton>
+        <CustomButton variant="contained">Picture C</CustomButton>
+      </Section>
+      <Section>
+        <Button variant="contained">Highscores!</Button>
+      </Section>
     </Base>
   );
 };
