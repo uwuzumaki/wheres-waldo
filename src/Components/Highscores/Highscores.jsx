aad2252e1654913828b6dc3edf4693dea76cafd4
Kris Wong <krischwong@gmail.com>
2025-09-17 22:29:23 -0400

    Very basic highscores page

--- src/Components/Highscores/Highscores.jsx
+++ src/Components/Highscores/Highscores.jsx
@@ -1,8 +1,50 @@
+import { styled } from "@mui/material";
+
+const Base = styled("div")(({ theme }) => ({
+  flex: 1,
+  display: "flex",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+  color: theme.palette.primary.main,
+}));
+
+const Player = styled("div")({
+  display: "flex",
+});
+
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 const Highscores = () => {
   return (
-    <>
-      <p>Highscores</p>
-    </>
+    <Base>
+      <div>
+        <p>Picture A</p>
+        {data.map((score) => (
+          <Player>
+            <p>name</p>
+            <p>{score}</p>
+          </Player>
+        ))}
+      </div>
+      <div>
+        <p>Picture b</p>
+        {data.map((score) => (
+          <Player>
+            <p>name</p>
+            <p>{score}</p>
+          </Player>
+        ))}
+      </div>
+      <div>
+        <p>Picture c</p>
+        {data.map((score) => (
+          <Player>
+            <p>name</p>
+            <p>{score}</p>
+          </Player>
+        ))}
+      </div>
+    </Base>
   );
 };
 
